e065a1a1edbce586697e73912811a11700d5b1d4
yarikkk13 <yaroslav.mahas@gmail.com>
2021-07-02 15:08:50 +0300

    hw04 3/3

--- lesson04/hw04.py
+++ lesson04/hw04.py
@@ -64,6 +64,85 @@ r2 = Rectangle(10, 12)
        "літак на 5 годин швидше за поїзд"
      - вивести всі дані про перевезення(поїзд) """
 
+
+class Transport:
+    def __init__(self, time, name):
+        self.time = time
+        self.name = name
+
+    def comparison(self, other):
+        if self.time < other.time:
+            print(f'{self.name} is faster for {other.time - self.time} hours than {other.name}')
+            return self.time - other.time
+        elif self.time > other.time:
+            print(f'{other.name} is faster for {self.time - other.time} hours than {self.name}')
+            return other.time - self.time
+        else:
+            print('they are both as fast as possible')
+            return 0
+
+
+class Car(Transport):
+    def __init__(self, passengers, time, fuels_price, km, name='car'):
+        super().__init__(time, name)
+        self.passengers = passengers
+        self.time = time
+        self.fuels_price = fuels_price
+        self.km = km
+        self.name = name
+
+    def cost(self):
+        return self.fuels_price * self.km
+
+    def comparison(self, other):
+        super().comparison(other)
+
+
+class Plane(Transport):
+    def __init__(self, fee, time, registration, travel_class, name='plane'):
+        super().__init__(time, name)
+        self.fee = fee
+        self.time = time
+        self.registration = registration
+        self.travel_class = travel_class
+        self.name = name
+
+    def general_time(self):
+        return self.registration + self.time
+
+    def comparison(self, other):
+        super().comparison(other)
+
+
+class Train(Transport):
+    def __init__(self, fee, time, place, name='train'):
+        super().__init__(time, name)
+        self.fee = fee
+        self.time = time
+        self.place = place
+        self.name = name
+
+    def __str__(self):
+        return str(self.__dict__)
+
+    def comparison(self, other):
+        super().comparison(other)
+
+
+car1 = Car(2, 5, 12, 123, 'chevrolet')
+car2 = Car(2, 10, 12, 123, 'zaz')
+train1 = Train(150, 24, 'upper', 'UZ')
+train2 = Train(450, 3, 'lux', 'Hyundai')
+plane1 = Plane(1200, 3, 2, 2, 'mau')
+plane2 = Plane(2400, 1, 1, 1, 'boeing')
+print(car1.cost())
+print(plane1.general_time())
+print(train1)
+car1.comparison(car2)
+car2.comparison(train2)
+train1.comparison(plane1)
+plane2.comparison(car1)
+
 """ 1)Створити пустий list
     2)Створити клас Letter
     3) створити змінну класу __count.
