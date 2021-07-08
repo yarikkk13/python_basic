4721beb0ec3ad39f2ef9caa85ebe8d2b5f0d500e
yarikkk13 <yaroslav.mahas@gmail.com>
2021-07-08 13:13:41 +0300

    lesson06

---
+++ lesson06/lesson06.py
@@ -0,0 +1,13 @@
+import os
+
+# print(os.uname())
+# print(os.environ)
+# os.system('ls -l')
+# print(os.getcwd())
+# print(os.listdir())
+# os.mkdir('111')
+# os.rename('lesson1.py', '111/lesson1.py')
+# for i in os.walk('.'):
+#     print(i)
+# join = os.path.join('111', 'lesson1.py')
+# print(join)
